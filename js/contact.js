ac323b7d7f68f47677e129a2d6390d920f551e12
mihaelsegulja <mihael.segulja@gmail.com>
2024-12-23 18:28:29 +0100

    Fix contact form not being sent properly
    Other
    - Visual changes for contact form
    - Visual changes for news
    - Add name atributes in login.html

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,6 @@
 
-$(document).ready(function () {
-    $.get("/views/contact.html", function (data) {
+$(document).ready(function() {
+    $.get("/views/contact.html", function(data) {
         $("body").append(data);
 
         $("#popup-container").dialog({
@@ -8,32 +8,64 @@ $(document).ready(function () {
             modal: true,
             show: {
                 effect: "drop",
+                direction: "up",
                 duration: 500
             },
             hide: {
                 effect: "drop",
+                direction: "up",
                 duration: 500
             },
             width: 400,
             buttons: {
-                "Pošalji": function () {
-                    alert("Form submitted!");
+                "Pošalji": function() {
+                    let fullName = $("#fullName").val();
+                    let email = $("#email").val();
+                    let importance = $("#importance").val();
+                    let message = $("#message").val();
+                    let recieveNewsletter = $("#recieveNewsletter").is(":checked");
+
+                    // console.log(fullName);
+                    // console.log(email);
+                    // console.log(importance);
+                    // console.log(message);
+                    // console.log(recieveNewsletter);
+
+                    if (!fullName || !email || !message){
+                        alert("Ispunite sva polja.");
+                        return;
+                    }
+
+                    $("#popup-form-content").trigger("submit");
+
                     $(this).dialog("close");
                 },
-                "Odustani": function () {
+                "Odustani": function() {
                     $(this).dialog("close");
                 }
             },
-            open: function () {
-                $(".ui-widget-overlay").hide().fadeIn(300);
+            open: function() {
+                $(".ui-widget-overlay").hide().fadeIn(500).on("click", function() {
+                    $("#popup-container").dialog("close");
+                });
             },
-            close: function () {
-                $(".ui-widget-overlay").fadeOut(300);
+            close: function() {
+                $(".ui-widget-overlay").fadeOut(500).off("click");
+            },
+            create: function() {
+                $(this)
+                    .closest(".ui-dialog")
+                    .find(".ui-dialog-title")
+                    .html(`
+                        <h2 style="font-size:1.5em;">
+                        <i class="nf nf-oct-mail"></i> 
+                        Kontaktirajte nas</h2>
+                    `);
             }
         });
     });
 
-    $("a[href='#contact']").on("click", function (e) {
+    $("a[href='#contact']").on("click", function(e) {
         e.preventDefault();
         $("#popup-container").dialog("open");
     });
