6de95af0fab8b7e164d7e6094a55656c4d1537cc
mihaelsegulja <mihael.segulja@gmail.com>
2025-01-17 11:59:26 +0100

    Display username on navbar when logged in

--- js/navbar.js
+++ js/navbar.js
@@ -12,27 +12,27 @@ jQuery(function($) {
         
         const curriculumLink = `
         <a href="/views/curriculum.html" id="curriculum">
-        <i class="nf nf-oct-mortar_board"></i> Nastavni plan
+            <i class="nf nf-oct-mortar_board"></i> Nastavni plan
         </a>
         `;
         const loginLink = `
         <a href="/views/login.html" id="login">
-        <i class="nf nf-oct-sign_in"></i> Prijava
+            <i class="nf nf-oct-sign_in"></i> Prijava
         </a>
         `;
         const logoutLink = `
         <a href="#" id="logout">
-        <i class="nf nf-oct-sign_out"></i> Odjava
+            <i class="nf nf-oct-sign_out"></i> Odjava: <b>${username}</b>
         </a>
         `;
         
         if (window.location.pathname === "/views/about.html") {
             const sectionLinks = `
             <div class="section-links animated-links">
-            <a href="#our-values">Naše vrijednosti</a>
-            <a href="#history">Povijest</a>
-            <a href="#algebra-group">Algebra grupa</a>
-            <a href="#find-us">Kako do nas?</a>
+                <a href="#our-values">Naše vrijednosti</a>
+                <a href="#history">Povijest</a>
+                <a href="#algebra-group">Algebra grupa</a>
+                <a href="#find-us">Kako do nas?</a>
             </div>
             `;
             $("nav").append(sectionLinks);
